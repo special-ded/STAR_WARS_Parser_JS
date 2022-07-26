93f920a63ee663412ae83ace9cc748b4146ce79b
Ivan <75171783+special-ded@users.noreply.github.com>
2022-07-26 11:46:33 +0300

    Update script.js
    Used Promises

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 let header = document.querySelector(".planets__table--head")
 let table = document.querySelector(".planets__table");
-let swAllPlanetsReq
+let swAllPlanetsObj
 let allPlanetsArr
 let residents = []
 let residentsURLs = []
 let swAllResidentsReq
+let residentObj
+let resObj = {}
 
-// GET ALL DATA FROM API
-let xhr = new XMLHttpRequest();
-xhr.open('GET', "https://swapi.dev/api/planets")
-xhr.send()
-xhr.onload = function () {
-  try {
-    swAllPlanetsReq = JSON.parse(xhr.response)
-  } catch (error) {
-    console.log(error)
-  }
-  allPlanetsArr = swAllPlanetsReq.results
-  createContentOfTable()
-}
+// // GET PLANET DATA FROM API
+fetch("https://swapi.dev/api/planets")
+  .then(response => response.json())
+  .then(data => swAllPlanetsObj = data)
+  .then(swAllPlanetsObj => allPlanetsArr = swAllPlanetsObj.results)
+  .then(() => console.log(allPlanetsArr))
+  .then(() => createContentOfTable())
 
 function createContentOfTable() {
   for (let i = 0; i < allPlanetsArr.length; i++) {
@@ -39,10 +35,10 @@ function createContentOfTable() {
     residentsURLs.push(allPlanetsArr[i].residents)
   }
   createHeaderOfTable()
-  foo()
+  checkForResidentsOnPlanet()
 }
 
-  // CREATE TABLE HEAD ====
+// CREATE TABLE HEAD ====
 function createHeaderOfTable() {
   let tHRow = header.insertRow(-1);
   // Insert a new cell (<td>) at the first position of the "new" <tr> element:
@@ -62,7 +58,7 @@ function createHeaderOfTable() {
   hcell4.innerHTML = keyOfObject[9].toUpperCase();
 }
 
-function foo() {
+function checkForResidentsOnPlanet() {
   for (let i = 0; i < residentsURLs.length; i++) {
     if (residentsURLs[i].length > 0) {
       residentsURLs[i].forEach(url =>
@@ -72,71 +68,48 @@ function foo() {
   }
 }
 
-let a = {}
+
 function getDataFromAPI(url) {
-  let residentXHR = new XMLHttpRequest();
-  residentXHR.open('GET', url)
-  residentXHR.send()
-
-  residentXHR.onload = function () {
-    try {
-      swAllResidentsReq = JSON.parse(residentXHR.response)
-    } catch (error) {
-      console.log(error)
-    }
-
-    a = swAllResidentsReq;
-    residents.push(a)
-    if (residents.length == 32) {
-      appendResidentsInTable()
-    }
-  }
+  fetch(url)
+    .then(response => response.json())
+    .then(data => swAllResidentsReq = data)
+    .then(() => resObj = swAllResidentsReq)
+    .then(resObj => residents.push(resObj))
+    .then(() => console.log(residents))
+    .then(() => appendResidentsInTable())
 }
 
 function appendResidentsInTable() {
-  console.log(residents)
   for (let j = 0; j < allPlanetsArr.length; j++) {
     let arr = []
     for (let i = 0; i < residents.length; i++) {
       if (residents[i].homeworld === `https://swapi.dev/api/planets/${j + 1}/`) {
         let cell5 = document.querySelector(`.residentLine${j}`)
         arr.push(`
-        <a onclick="getResidentData('${residents[i].url}')"
-        href='#'>   
-        ${residents[i].name}
-        </a>`)
+          <a onclick="getResidentData('${residents[i].url}')"
+          href='#'>   
+          ${residents[i].name}
+          </a>`)
         cell5.innerHTML = arr
       }
     }
   }
 }
 
+// GET RESIDENT DATA FROM API FOR CHOSEN RESIDENT TABLE
 
-let residentObj
 function getResidentData(residentURL) {
-  console.log(residentURL)
-
-  // GET RESIDENT DATA FROM API FOR TABLE
-  let ResidentTablexhr = new XMLHttpRequest();
-  ResidentTablexhr.open('GET', residentURL)
-  ResidentTablexhr.send()
-  ResidentTablexhr.onload = function () {
-    try {
-      residentObj = JSON.parse(ResidentTablexhr.response)
-    } catch (error) {
-      console.log(error)
-    }
-    console.log(residentObj)
-
-    createContentOfResidentTable(residentObj)
-  }
+  fetch(residentURL)
+    .then(response => response.json())
+    .then(response => residentObj = response)
+    .then(() => createContentOfResidentTable(residentObj))
 }
+
 let residentTable = document.querySelector(".residents__table");
 
+// CREATE CHOSEN RESIDENT TABLE CONTENT
 
 function createContentOfResidentTable(residentObj) {
-
-  // CREATE TABLE CONTENT
   let row = residentTable.insertRow(-1);
   let cell1 = row.insertCell(0);
   let cell2 = row.insertCell(1);
